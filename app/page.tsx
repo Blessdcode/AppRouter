6ef7ff3c69672e3ba6e35bb060cfb53d6c973ed9
Blessdbnjmn <113176355+Blessdcode@users.noreply.github.com>
2025-01-09 14:44:13 +0100

    added the mapbox api

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,33 @@
 // import Image from "next/image";
 
 import Banner from "@/components/banner.client";
+import Card from "@/components/card.server";
+import { fetchCoffeeStores } from "@/lib/coffee.stores";
+import { CoffeeStoreType } from "@/types";
 import Link from "next/link";
 
-export default function Home() {
-  const coffeeStoreId ="dark-horse-coffee"
+
+
+async function getData() {
+  return await fetchCoffeeStores();
+}
+
+export default async function Home() {
+  const coffeeStores = await getData();
+
   return (
     <main className="mx-auto mt-10 max-w-6xl px-4">
       <Banner />
-      <Link href={`/coffe-store/${coffeeStoreId}`}>Dark horse coffee</Link>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-2 lg:grid-cols-3 lg:gap-6">
+        {coffeeStores.map((coffeeStore: CoffeeStoreType) => (
+          <Card
+            key={`${coffeeStore.id}`}
+            name={coffeeStore.name}
+            imgUrl={coffeeStore.imgUrl}
+            href={`/coffee-store/${coffeeStore.id}`}
+          />
+        ))}
+      </div>
     </main>
   );
 }
---
+++ types/index.ts
@@ -0,0 +1,13 @@
+export type CoffeeStoreType = {
+  id: string;
+  name: string;
+  imgUrl: string;
+};
+
+export type MapboxType = {
+  id: string;
+  properties: {
+    address: string;
+  };
+  text: string;
+};
